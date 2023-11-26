09b41939b56c926d071ae098f30b6fbeef377855
Neil Smith <neil@nsmith.co.uk>
2023-11-26 16:58:05 +0000

    Stop padding the base64

--- tssig/b64bytes.go
+++ tssig/b64bytes.go
@@ -6,8 +6,8 @@ import "encoding/base64"
 type b64bytes []byte
 
 func (s b64bytes) MarshalJSON() ([]byte, error) {
-	encodedVal := make([]byte, base64.URLEncoding.EncodedLen(len(s)))
-	base64.URLEncoding.Encode(encodedVal, s)
+	encodedVal := make([]byte, base64.RawURLEncoding.EncodedLen(len(s)))
+	base64.RawURLEncoding.Encode(encodedVal, s)
 
 	// +2 to cover the quote marks each side
 	result := make([]byte, 0, len(encodedVal)+2)
@@ -24,8 +24,8 @@ func (s *b64bytes) UnmarshalJSON(data []byte) error {
 	// Remove the quote marks (") - the first and last bytes
 	dataWithoutQuotes := data[1 : len(data)-1]
 
-	buffer := make(b64bytes, base64.URLEncoding.DecodedLen(len(dataWithoutQuotes)))
-	l, err := base64.URLEncoding.Decode(buffer, dataWithoutQuotes)
+	buffer := make(b64bytes, base64.RawURLEncoding.DecodedLen(len(dataWithoutQuotes)))
+	l, err := base64.RawURLEncoding.Decode(buffer, dataWithoutQuotes)
 
 	// The original buffer is often too big, thus we only return the length l decoded - the relevant bytes.
 	*s = buffer[:l]
